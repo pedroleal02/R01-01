9061060e2be509b23c6299f9f8602d43e2b8c250
pedro <pedro.leal.e.lima@ccc.ufcg.edu.br>
2025-11-17 15:03:57 -0300

    envio do roteiro 01 finalizado 2.0

--- src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
+++ src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
@@ -16,13 +16,27 @@ public class BidirectionalBubbleSort<T extends Comparable<T>> extends
 		@Override
 		public void sort(T[] array, int leftIndex, int rightIndex) {
 			if (array != null && leftIndex >= 0 && rightIndex <= array.length) {
-				for (int i = 0; i < rightIndex; i++) {
-					for (int j = leftIndex; j < rightIndex - i - 1; j++) {
-						if (array[j].compareTo(array[j+1]) > 0) Util.swap(array, j, j+1);
+				boolean swapped = true;
+				while (swapped) {
+					swapped = false;
+					for (int i = leftIndex; i < rightIndex; i++) {
+						if (array[i].compareTo(array[i + 1]) > 0) {
+							Util.swap(array, i, i + 1);
+							swapped = true;
+						}
 					}
-					for (int z = rightIndex; z > leftIndex + i + 1; z--) {
-						if (array[z].compareTo(array[z-1]) < 0) Util.swap(array, z, z-1);
+					rightIndex--;
+					if (!swapped) {
+						continue;
 					}
+					swapped = false;
+					for (int i = rightIndex; i > leftIndex; i--) {
+						if (array[i].compareTo(array[i - 1]) < 0) {
+							Util.swap(array, i, i - 1);
+							swapped = true;
+						}
+					}
+					leftIndex++;
 				}
 			}
 		}
